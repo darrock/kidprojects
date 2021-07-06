c41c680e6e5c450e7ea89b6466f07dd0ac68291e
gameroom <gameroom@gameroom-mini.local>
2021-07-06 13:18:43 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,100 @@
+print("Darrock is the coolest kid around")
+print("He is learning how to code python")
+
+x = 2
+y = 3
+answer = x * y
+print(x*y)
+
+print("x = ", x)
+print("y = ", y)
+print("x * y = ", answer)
+
+loop = (1,2,3,4,5,6,7,8,9,10)
+for i in loop:
+    print(i)
+
+print("hello world")
+
+print("i'm learning how to code in python")
+
+print("\"kumusta\" is \"hello\" in Tagalog")
+
+print("  ")
+
+print("you need \na specific \nformat to \nwrite different \nwords on \ndifferent lines")
+
+print("  ")
+
+Father = "Dad"
+
+codingSon = "Darrock"
+
+print(codingSon, "loves his", Father)
+
+print("  ")
+
+code = "f strings"
+
+print(f"my book says to use {code} in order to use variables in a sentence")
+
+multiSentence = """
+you need
+a specific
+format to
+write different
+words on
+different lines"""
+
+print(multiSentence)
+
+print("(these are activities my book assigned me) \n")
+
+print("Hi! my name is Darrock \n")
+
+print("\"python coding is so much fun, plus it teaches you many things\" -Darrock"
+      "\n")
+
+
+mood = "joyful"
+
+print(f"today I feel {mood} \n")
+
+print("the wind blows gently \nthrough the flowers, trees and vines \nas the forest sleeps \n")
+
+nameOfPerson = " "
+
+adjective = " "
+
+favoriteFood = " "
+
+number = " "
+
+typeOfTree = " "
+
+sillyStory = f"""
+Hi, my name is {nameOfPerson}
+I really like {adjective} {favoriteFood}
+I like it so much I eat it least {number} times
+every day
+it tastes even better under a {typeOfTree}
+"""
+
+print(sillyStory,"\n")
+
+firstName = "Darrock"
+
+fullName = f"{firstName} O'Neill"
+
+print(f"{firstName} \n{fullName} \n")
+
+cake = """
+     | | | |
+    (@@@@@@@)
+   (@@@@@@@@@)
+  (@@@@@@@@@@@)
+ (@@@@@@@@@@@@@)
+(@@@@@@@@@@@@@@@)
+"""
+
+print(cake)
